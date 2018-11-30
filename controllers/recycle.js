2bdef78ae9e1495dfd193cc4d26b8e1e8643a237
Andrew <andy.birks@gmail.com>
2018-11-30 22:42:13 +0000

    Installing Cloudinary - Uploading Image and sending Via WhatsApp

--- controllers/recycle.js
+++ controllers/recycle.js
@@ -1,14 +1,24 @@
 const puppeteer = require("puppeteer");
+const cloudinary = require("cloudinary");
+const twilio = require("twilio")(
+  "AC8f9efa9754e44d893b46b0f605822667",
+  "b472e8159e3e23add20b45916297cced"
+);
+
+cloudinary.config({
+  cloud_name: "birksy",
+  api_key: "916718182388156",
+  api_secret: "4VuxM6M8c7LBZ3wHjqOXItJAb_o"
+});
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false
+    //headless: false
   });
   const page = await browser.newPage();
   await page.goto(
     "https://www.darlington.gov.uk/environment-and-planning/street-scene/weekly-refuse-and-recycling-collection-lookup/"
   );
-
 
   //Register Values to be used
   const POSTCODE = "DL2 2GE";
@@ -34,10 +44,38 @@ const puppeteer = require("puppeteer");
   await page.click(DOM_ADDRESS_SUBMIT);
   await page.waitForNavigation();
 
-  const RESULTS = await page.$('#detailsDisplay');
+  const RESULTS = await page.$("#detailsDisplay");
 
-  await RESULTS.screenshot({ path: `../public/${UPRN}-${Date.now()}.png` });
+  const SCREENSHOT_PATH = `../public/${UPRN}-${Date.now()}.png`;
 
+  await RESULTS.screenshot({ path: SCREENSHOT_PATH });
 
   await browser.close();
+
+  //Send Image up to cloudinary
+  var cloudinaryUpload = await cloudinary.v2.uploader.upload(
+    SCREENSHOT_PATH,
+    function(error, result) {
+      //console.log(result, error);
+
+      //If the file has been uploaded to cloudinary - Delete from local
+      var fs = require("fs");
+      var filePath = SCREENSHOT_PATH;
+      fs.unlinkSync(filePath);
+
+      return result;
+    }
+  );
+
+  console.log(cloudinaryUpload.secure_url);
+
+  const message = {
+    to: "whatsapp:+447799061149",
+    from: "whatsapp:+14155238886",
+    body: `New Recycling Message...`,
+    mediaUrl: cloudinaryUpload.secure_url
+  };
+  twilio.messages.create(message).then(sentMessage => {
+    console.log(`Text send to ${sentMessage.to}`);
+  });
 })();
