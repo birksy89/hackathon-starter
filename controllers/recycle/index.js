255933c01f970601c748f789e2a15fe0cca096fd
Andrew <andy.birks@gmail.com>
2019-01-13 21:41:14 +0000

    Fixing Schedule Error - Should be Min not sec

--- controllers/recycle/index.js
+++ controllers/recycle/index.js
@@ -172,7 +172,7 @@ exports.getAddressFromPostcode = async (req, res, next) => {
  │    └──────────────────── minute (0 - 59)
 └───────────────────────── second (0 - 59, OPTIONAL)
 */
-const j = schedule.scheduleJob({ hour: 17, second: 30 }, () => {
+const j = schedule.scheduleJob({ hour: 17, minute: 30 }, () => {
   module.exports.checker();
 });
 
